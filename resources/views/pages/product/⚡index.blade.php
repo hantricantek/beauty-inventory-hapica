d83bfc7f0ee61781db3b348ef2e7ed408c58b867
balinux <ryojuniyantara@gmail.com>
2026-06-13 16:11:26 +0800

    menambah text h1

--- resources/views/pages/product/⚡index.blade.php
+++ resources/views/pages/product/⚡index.blade.php
@@ -2,21 +2,19 @@
 
 use Livewire\Component;
 
-new class extends Component
-{
+new class extends Component {
     //
 };
 ?>
 
-<div >
+<div>
     <flux:modal.trigger name="create-product">
-    <flux:button
-        variant="primary"
-        icon="plus"
-    >
-        Add Product
+        <flux:button variant="primary" icon="plus">
+            Add Product
 
-    </flux:button>
+        </flux:button>
     </flux:modal.trigger>
     <livewire:product.create />
+
+    <h1>Hello balinux was here</h1>
 </div>
