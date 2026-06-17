2cbe9aea659b340cd73c8e0492f6c64d88ae3c1d
Maershaa123 <maershahelena.240604070@student.hamzanwadi.ac.id>
2026-06-17 16:55:57 +0800

    ditambahkan oleh maersha

--- resources/views/pages/suppliers/index.blade.php
+++ resources/views/pages/suppliers/index.blade.php
@@ -11,6 +11,16 @@ new class extends Component
     {
         return Supplier::latest()->get();
     }
+
+    public function edit($id)
+    {
+        $this->dispatch('edit-supplier', id: $id);
+    }
+
+    public function delete($id)
+    {
+        $this->dispatch('confirm-delete-supplier', id: $id);
+    }
 };
 
 ?>
@@ -27,14 +37,44 @@ new class extends Component
 
     <flux:separator variant="subtle" />
 
+    <!-- Button Add Supplier -->
+    <flux:modal.trigger name="create-supplier">
+        <flux:button
+            variant="primary"
+            icon="plus"
+        >
+            Add Supplier
+        </flux:button>
+    </flux:modal.trigger>
+
+    <!-- Modal Create -->
+    <livewire:supplier.create />
+
+    <!-- Modal Edit -->
+    <livewire:supplier.edit />
+
     <div class="overflow-x-auto">
 
         <flux:table>
 
             <flux:table.columns>
-                <flux:table.column>Name</flux:table.column>
-                <flux:table.column>Phone</flux:table.column>
-                <flux:table.column>Address</flux:table.column>
+
+                <flux:table.column>
+                    Name
+                </flux:table.column>
+
+                <flux:table.column>
+                    Phone
+                </flux:table.column>
+
+                <flux:table.column>
+                    Address
+                </flux:table.column>
+
+                <flux:table.column>
+                    Action
+                </flux:table.column>
+
             </flux:table.columns>
 
             <flux:table.rows>
@@ -53,6 +93,41 @@ new class extends Component
 
                         <flux:table.cell>
                             {{ $supplier->address }}
+                        </flux:table.cell>
+
+                        <flux:table.cell>
+
+                            <flux:dropdown>
+
+                                <flux:button
+                                    variant="ghost"
+                                    size="sm"
+                                    icon="ellipsis-horizontal"
+                                />
+
+                                <flux:menu>
+
+                                    <flux:menu.item
+                                        icon="pencil"
+                                        wire:click="edit({{ $supplier->id }})"
+                                    >
+                                        Edit
+                                    </flux:menu.item>
+
+                                    <flux:menu.separator />
+
+                                    <flux:menu.item
+                                        variant="danger"
+                                        icon="trash"
+                                        wire:click="delete({{ $supplier->id }})"
+                                    >
+                                        Delete
+                                    </flux:menu.item>
+
+                                </flux:menu>
+
+                            </flux:dropdown>
+
                         </flux:table.cell>
 
                     </flux:table.row>
