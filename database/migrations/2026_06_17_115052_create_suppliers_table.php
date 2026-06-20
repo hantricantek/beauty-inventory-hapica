45ce8cb602c522c7b5a6dc6c76bc3c4bdba7e676
Maershaa123 <maershahelena.240604070@student.hamzanwadi.ac.id>
2026-06-20 14:36:14 +0800

    update maersha

--- database/migrations/2026_06_17_115052_create_suppliers_table.php
+++ database/migrations/2026_06_17_115052_create_suppliers_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique()->nullable();
             $table->string('phone')->nullable();
             $table->text('address')->nullable();
             $table->timestamps();
